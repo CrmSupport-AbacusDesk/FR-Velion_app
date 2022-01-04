2b1d845f745d07a8145d4bba0bd4a2701c201e08
SatyamAbacus <78525156+SatyamAbacus@users.noreply.github.com>
2022-01-04 15:38:39 +0530

    satyam

--- www/js/app/global.js
+++ www/js/app/global.js
@@ -1,8 +1,8 @@
 
 var db;
 var backbutton = 0;
-var rootURL = "http://microtek.abacusdesk.com";
-// var rootURL = "http://phpstack-83335-1572480.cloudwaysapps.com";
+// var rootURL = "http://microtek.abacusdesk.com";
+var rootURL = "http://phpstack-83335-1572480.cloudwaysapps.com";
 // var rootURL = "http://localhost/okaya/okaya-sfa-web";
 var serverURL = rootURL+"/index.php/app";
 var sfaServerURL = rootURL+"/index.php/app/okaya_sfa";
